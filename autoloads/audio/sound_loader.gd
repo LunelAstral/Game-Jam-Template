22de69033e907fb29d5238777f21c69ad4ecd90a
Rile-Cry <rileycriddle@yahoo.com>
2025-04-24 14:04:14 -0400

    ReadMe And SoundLoader Update

--- autoloads/audio/sound_loader.gd
+++ autoloads/audio/sound_loader.gd
@@ -1,5 +1,7 @@
 class_name SoundLoader
 
+# How setup work is within _list you preload the sound so it's an AudioStream
+
 func load_audio() -> void:
 	load_ost()
 	load_sfx()
@@ -24,11 +26,19 @@ func load_sfx() -> void:
 
 func load_ui() -> void:
 	# Add UI loads here
-	pass
+	var ui_list : Array[AudioStream] = []
+	
+	for ui in ui_list:
+		ui.resource_name = grab_name(ui.resource_path)
+		SoundManager.add_sound(ui)
 
 func load_ambient() -> void:
 	# Add Ambient loads here
-	pass
+	var ambient_list : Array[AudioStream] = []
+	
+	for ambient in ambient_list:
+		ambient.resource_name = grab_name(ambient.resource_path)
+		SoundManager.add_sound(ambient)
 
 func grab_name(path: String) -> String:
 	return path.split("/")[path.split("/").size() - 1].split(".")[0]
