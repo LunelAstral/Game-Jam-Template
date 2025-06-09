afe8f578ba14d929546cee1e4d024aa89c3382ea
Rile-Cry <rileycriddle@yahoo.com>
2025-06-09 18:26:06 -0400

    Debug

---
+++ autoloads/debug/debug.gd
@@ -0,0 +1,55 @@
+extends CanvasLayer
+
+#region Variables
+@export var text_box : TextEdit
+@export var text_line : LineEdit
+
+var text : String = "Debug Terminal"
+#endregion
+
+#region Built-Ins
+func _ready() -> void:
+	text_box.set_line(0, text)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"debug"):
+		if visible:
+			hide()
+		else:
+			show()
+#endregion
+
+#region Signal Callbacks
+func _on_text_submit(new_text: String) -> void:
+	text_line.text = ""
+	if new_text[0] == "/":
+		var command_text := new_text.split("/")[1]
+		var command := command_text.split(" ")
+		var command_name := command[0]
+		var args : Array[String] = []
+		if command.size() > 1:
+			args = command.slice(1)
+		
+		if self.has_method(command_name):
+			var callable = Callable(self, command_name)
+			callable.call(args)
+		else:
+			text_box.set_line(text_box.get_line_count(), "There is no command by the name %s" % command_name)
+	else:
+		text_box.set_line(text_box.get_line_count(), "That was not a command, please use /")
+	
+	text_box.text = text
+	text_box.scroll_vertical = text_box.get_line_count() - 1
+#endregion
+
+#region Commands
+func help(args: Array[String]) -> void:
+	if args.size() == 0:
+		var command_list : Array[String] = [
+			"help"
+		]
+		
+		text += "\nList of Available Commands:"
+		for command in command_list:
+			text += "\n%s" % command
+#endregion
